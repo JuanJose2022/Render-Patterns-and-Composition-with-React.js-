c99af0679d3834dce019209b1505a8d815acf1ea
unknown <yusquen22@gmail.com>
2024-09-15 15:26:52 -0500

    Creating your first component Higher-Order Components (HOC)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+//import App from './App/index.js';
 import './index.css';
 
+function App(props) {
+  return (
+    <h2>!{props.saludo}, {props.nombre}¡</h2>
+  );
+}
+
+function withSaludo(WrapperdComponent) {
+  return function WrapperdComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad (props) {
+      return (
+        <React.Fragment>
+          <WrapperdComponent {...props} saludo={saludo}/>
+          <p>Estamos acompañamdo al Wrapperdomponent</p>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+const AppwithSaludo = withSaludo(App)
+const AppwithSaludo2 =AppwithSaludo('Buenass');
+const AppwithSaludo3 =AppwithSaludo('Buenass amigos');
+
+
+
 ReactDOM.render(
-  <App />,
+  <React.Fragment>
+    <AppwithSaludo2 nombre={"Alex"} />
+    <AppwithSaludo3 nombre={"jose"} />
+  </React.Fragment>,
   document.getElementById('root')
 );
