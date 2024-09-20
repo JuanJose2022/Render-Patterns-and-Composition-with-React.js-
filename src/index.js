409de342e361d93a29624b7cb335a6fa55b0df34
unknown <yusquen22@gmail.com>
2024-09-20 18:53:09 -0500

    Notifying changes with StorageEventListener

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App/index.js';
+import App from './App/index.js';
 import './index.css';
 
-function App(props) {
-  return (
-    <h2>!{props.saludo}, {props.nombre}¡</h2>
-  );
-}
-
-function withSaludo(WrapperdComponent) {
-  return function WrapperdComponentWithSaludo(saludo) {
-    return function ComponenteDeVerdad (props) {
-      return (
-        <React.Fragment>
-          <WrapperdComponent {...props} saludo={saludo}/>
-          <p>Estamos acompañamdo al Wrapperdomponent</p>
-        </React.Fragment>
-      );
-    }
-  }
-}
-
-const AppwithSaludo = withSaludo(App)
-const AppwithSaludo2 =AppwithSaludo('Buenass');
-const AppwithSaludo3 =AppwithSaludo('Buenass amigos');
-
-
-
 ReactDOM.render(
-  <React.Fragment>
-    <AppwithSaludo2 nombre={"Alex"} />
-    <AppwithSaludo3 nombre={"jose"} />
-  </React.Fragment>,
+  <App />,
   document.getElementById('root')
 );
